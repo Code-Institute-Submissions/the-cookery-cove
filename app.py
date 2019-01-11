6dc40e921133e16d89e85637fe0803f11fc63c91
Kim Pearton <kim.pearton@live.co.uk>
2019-01-11 16:22:59 +0000

    Connect Flask to MongoDB

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 import os
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'online_cookbook'
+app.config["MONGO_URI"] = 'mongodb://admin:cook1book2@ds129914.mlab.com:29914/online_cookbook'
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
